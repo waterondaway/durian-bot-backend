c5ecf1f6e9d323f77be8674d221e2c5ba4f53fae
Nonpawit Silabumrungrad <nonpawit.sila@gmail.com>
2025-01-31 14:49:15 +0700

    Add folderRoute.js

--- controllers/folderControl.js
+++ controllers/folderControl.js
@@ -1,7 +1,7 @@
 const pool = require("../models/db");
 exports.getAllFolders = async (req, res) => {
   try {
-    const [rows] = await pool.query("SELECT id, farmer_name, farmer_id, DATE_FORMAT(last_update, '%Y-%m-%d') AS last_update FROM folder_farmer");
+    const [rows] = await pool.query("SELECT id, farmer_name, farmer_id, DATE_FORMAT(last_update, '%Y-%m-%d') AS last_update FROM farmer WHERE status != 0");
     res.status(200).json(rows);
   } catch (err) {
     console.error(err);
@@ -24,3 +24,19 @@ exports.getImageFolderID = async (req, res) => {
     return res.status(500).json({ message: `${err}` });
   }
 };
+
+exports.getPropertiesFolderID = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [rows] = await pool.query("SELECT id, farmer_name, farmer_id, user_id, status, organization, telephone,  DATE_FORMAT(last_update, '%Y-%m-%d') AS last_update FROM farmer WHERE farmer_id = ?", [id], (err, result) => {
+      if (err) {
+          return res.status(500).json({ message: 'Error retrieving data' });
+      }
+      return res.status(200).json(result);
+    });
+    res.status(200).json(rows);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: `${err}` });
+  }
+  };
--- routes/folderRoute.js
+++ routes/folderRoute.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const { getAllFolders, getImageFolderID } = require('../controllers/folderControl');
+const { getAllFolders, getImageFolderID, getPropertiesFolderID } = require('../controllers/folderControl');
 
 router.get('/folders', getAllFolders)
 router.get('/folders/:id', getImageFolderID)
+// router.get('/foldersProperties/:id', getPropertiesFolderID)
 module.exports = router
